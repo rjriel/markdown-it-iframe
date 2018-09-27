d1768e054395d224f33ee9cabcacf308c2c3e165
rjriel <rie@qlik.com>
2018-09-27 09:41:48 -0400

    lint fixing

--- test/test.js
+++ test/test.js
@@ -1,14 +1,12 @@
-'use strict';
+"use strict"
 
-
-var path     = require('path');
-var generate = require('markdown-it-testgen');
+var path = require("path")
+var generate = require("markdown-it-testgen")
 
 /*eslint-env mocha*/
 
-describe('markdown-it-iframe', function () {
-  var md = require('markdown-it')({ linkify: true })
-              .use(require('../'));
+describe("markdown-it-iframe", function() {
+  var md = require("markdown-it")({ linkify: true }).use(require("../"))
 
-  generate(path.join(__dirname, 'fixtures/iframe.txt'), md);
-});
+  generate(path.join(__dirname, "fixtures/iframe.txt"), md)
+})
